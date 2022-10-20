ecc29fc5644d1f23a50f65b477f13e807bfa4068
Tomasz Witkowski <witkowskitomasz@protonmail.com>
2022-10-20 21:14:11 +0200

    new ui

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-
-const weekNum = document.getElementById('week_num');
-
+const monthAndYearDisplay = document.querySelector('.month-and-year');
+const weekDisplay = document.querySelector('.week');
 
 const monday = document.getElementById('mon');
-const tuesday = document.getElementById('tues');
-const wedensday = document.getElementById('wed');
-const thursday = document.getElementById('thurs');
+const tuesday = document.getElementById('tue');
+const wednesday = document.getElementById('wed');
+const thursday = document.getElementById('thu');
 const friday = document.getElementById('fri');
 const saturday = document.getElementById('sat');
 const sunday = document.getElementById('sun');
@@ -17,33 +16,67 @@ dayjs.extend(window.dayjs_plugin_weekday);
 
 
 
-const WeekCalendar = {
-    todayFullDate: dayjs().format("D MM YYYY"),
-    todayDay: dayjs().format("D"),
-    todayMonth: dayjs().format("MM"),
-    
-    weekDayNumber: dayjs().isoWeekday(),
-    weekNumber: dayjs().isoWeek(),
-
-
-
-    setWeek() {
-        weekNum.textContent = this.weekNumber;
-    },
-
-    setDays() {
-        monday.textContent = dayjs().weekday(1).$D;
-        tuesday.textContent = dayjs().weekday(2).$D;
-        wedensday.textContent = dayjs().weekday(3).$D;
-        thursday.textContent = dayjs().weekday(4).$D;
-        friday.textContent = dayjs().weekday(5).$D;
-        saturday.textContent = dayjs().weekday(6).$D;
-        sunday.textContent = dayjs().weekday(7).$D;
-    }
+const main = () => {
+    setCurrentWeek();
+    setCurrentMonthAndYear();
+    SetCurrentDays();
+    setToday();
 }
 
-WeekCalendar.setWeek();
-WeekCalendar.setDays();
+
+const setCurrentWeek = () => {
+    weekDisplay.textContent = `Week: ${dayjs().isoWeek()}`  
+}
+
+const setCurrentMonthAndYear = () => {
+    monthAndYearDisplay.textContent = dayjs().format('MMMM YYYY');
+}
+
+const SetCurrentDays = () => {
+    firstDayOfTheWeek = dayjs().startOf('isoWeek').$D;
+    
+    monday.textContent = firstDayOfTheWeek;
+    tuesday.textContent = firstDayOfTheWeek + 1;
+    wednesday.textContent = firstDayOfTheWeek + 2;
+    thursday.textContent = firstDayOfTheWeek + 3;
+    friday.textContent = firstDayOfTheWeek + 4;
+    saturday.textContent = firstDayOfTheWeek + 5;
+    sunday.textContent = firstDayOfTheWeek + 6;
+}
+
+const setToday = () => {
+    const today = dayjs().format("D"); 
+    
+    const days = [
+        monday,
+        tuesday,
+        wednesday,
+        thursday,
+        friday,
+        saturday,
+        sunday
+    ]
+
+    days.forEach(element => {
+        let day = element;
+        if (day.textContent == today) {
+            day.classlist.add("today");
+        }
+
+    });
+}
+
+
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', main);
+
+
 
 
 
