f10a8fd385a698de62ae13b1a4c21bdd8e1b22b2
Tomasz <witkowskitomasz@protonmail.com>
2022-10-24 14:08:29 +0200

    current week is in array

--- app.js
+++ app.js
@@ -1,37 +1,51 @@
-
-
-
-
-
 dayjs.extend(window.dayjs_plugin_isoWeek);
 dayjs.extend(window.dayjs_plugin_weekday);
 dayjs.extend(window.dayjs_plugin_dayOfYear);
 
 
-
-
-
 // Main function on window start
+
 const main = () => {
     
 }
 
-// Makes array with days in current week in format - YYYY-MM-D !!GIVES ERROR!!
-const getsCurrentWeekDays = () => {
-    const week = [];
-    const currentDayNum = startOfTheWeekDayInYear();
 
-        for (let i = 0; i = 6; i++) {
-            let day = currentDayNum + i ;
-            let date = dayjs().day(day);
-            console.log(day);
-            console.log(date);
-        }
-    console.log(week);
+
+
+
+
+
+
+
+
+// Makes array with days in current week in format - YYYY-MM-D
+
+const getsCurrentWeekDays = () => {
     
+    let week = [];
+    const currentDayNum = startOfTheWeekDayInYear();
+    
+        for (let i = 0; i < 7; i++) {
+            
+            let day = currentDayNum + i;
+            let date = dayjs().dayOfYear(day);
+            let date2 = `${date.$y}-${date.$M + 1}-${date.$D}`;
+            
+            week.push(date2);
+            
+        }
+    return week;
+}
+
+// Start of week day of year
+
+const startOfTheWeekDayInYear = () => {
+    const dayInYear = dayjs(startOfTheWeekDate()).dayOfYear();
+    return dayInYear;
 }
 
 // Start of week date in format - YYYY-MM-D
+
 const startOfTheWeekDate = () => {
     const startDay = dayjs().startOf('isoWeek').$D;
     const startMonth = dayjs().startOf('isoWeek').$M + 1;
@@ -40,19 +54,7 @@ const startOfTheWeekDate = () => {
     return result;
 }
 
-// Start of week day of year
-const startOfTheWeekDayInYear = () => {
-    const dayInYear = dayjs(startOfTheWeekDate()).dayOfYear();
-    return dayInYear;
-}
-
-
-
-
-
-
-
-
+// -----------------------------
 
 document.addEventListener('DOMContentLoaded', main);
 
